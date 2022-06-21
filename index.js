0d0bae38f7543d8f23c3d8edbab9e5d3b7f9ec8c
K-Carty <kieran.carty@me.com>
2022-06-21 14:38:04 +0100

    testing to be set-up

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/set', (req,res) => {
+  const somevalue = req.query.somekey;
+  app.set('somekey', somevalue)
+  res.send({ 'somevalue': somevalue })
+})
+
+app.get('/get', (req,res) => {
+  const somekey = req.query.key
+  res.send(app.get(somekey))
+})
+
+app.listen(port, () => { 
+  console.log(`DB server app listening at http://localhost:${port}`)
+});
